2f686a5447cae063c24e71e94a00a8a460bb30b9
wit543 <norawit543@hotmail.com>
2014-11-24 11:31:29 +0700

    a

--- src/RounfApp.java
+++ src/RounfApp.java
@@ -2,7 +2,7 @@
 public class RounfApp {
 public static void main(String[] args){
 	Round r = new Round(new Theater(new int[]{3,5,4},8, 3, new double[]{1,200,3},new String[]{"regular","premium","honeymoon"}),new Time(20, 30, 0),new Time(23,00,00),new Movie());
-	System.out.println(r.getSeatInString(2, 0 ,1));
+	System.out.println(r.getSeatInString(2, 0 ,0));
 	System.out.println(r.convert(10, 0));
 }
 }
--- src/Ticket.java
+++ src/Ticket.java
@@ -2,14 +2,15 @@
 public class Ticket {
 private Movie movie;
 private Round round;
-private String movieTitle,date,seat,ID,theater;
-private int numberOfTicket=1;
+private String movieTitle,date,seatNumber,ID,theater,cinemaName;
+
+private static int numberOfTicket=1;
 double price, discount;
  
-Ticket(String movieTitle,String date ,String seat,String theater){
+Ticket(Movie movie,Round round,String seat){
 	this.movieTitle = movieTitle;
 	this.date = date;
-	this.seat = seat;
+	this.seatNumber = seatNumber;
 	this.theater = theater;
 }
 }
